3ba9c4cff4de828f335409d4b239186b8ead55d0
sixone-Jiang <326896161@qq.com>
2023-02-10 16:41:49 +0800

    Site updated: 2023-02-10 16:41:48

--- js/main.js
+++ js/main.js
@@ -1,123 +1,96 @@
-// 监听滚动事件
-function listenScroll(callback) {
-  // eslint-disable-next-line no-undef
-  const dbc = new Debouncer(callback);
-  window.addEventListener('scroll', dbc, false);
-  dbc.handleEvent();
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>// inner variables
+var canvas, ctx;
+var clockRadius = 250;
+var clockImage;
+
+// draw functions :
+function clear() { // clear canvas function
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
-// 滚动到指定元素
-function scrollToElement(target, offset) {
-  var scroll_offset = $(target).offset();
-  $('body,html').animate({
-    scrollTop: scroll_offset.top + (offset || 0),
-    easing   : 'swing'
-  });
-}
+function drawScene() { // main drawScene function
+    clear(); // clear canvas
 
-// 顶部菜单的监听事件
-function navbarScrollEvent() {
-  var navbar = $('#navbar');
-  var submenu = $('#navbar .dropdown-menu');
-  if (navbar.offset().top > 0) {
-    navbar.removeClass('navbar-dark');
-    submenu.removeClass('navbar-dark');
-  }
-  listenScroll(function() {
-    navbar[navbar.offset().top > 50 ? 'addClass' : 'removeClass']('top-nav-collapse');
-    submenu[navbar.offset().top > 50 ? 'addClass' : 'removeClass']('dropdown-collapse');
-    if (navbar.offset().top > 0) {
-      navbar.removeClass('navbar-dark');
-      submenu.removeClass('navbar-dark');
-    } else {
-      navbar.addClass('navbar-dark');
-      submenu.removeClass('navbar-dark');
+    // get current time
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    hours = hours > 12 ? hours - 12 : hours;
+    var hour = hours + minutes / 60;
+    var minute = minutes + seconds / 60;
+
+    // save current context
+    ctx.save();
+
+    // draw clock image (as background)
+    ctx.drawImage(clockImage, 0, 0, 500, 500);
+
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.beginPath();
+
+    // draw numbers
+    ctx.font = '36px Arial';
+    ctx.fillStyle = '#000';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    for (var n = 1; n <= 12; n++) {
+        var theta = (n - 3) * (Math.PI * 2) / 12;
+        var x = clockRadius * 0.7 * Math.cos(theta);
+        var y = clockRadius * 0.7 * Math.sin(theta);
+        ctx.fillText(n, x, y);
     }
-  });
-  $('#navbar-toggler-btn').on('click', function() {
-    $('.animated-icon').toggleClass('open');
-    $('#navbar').toggleClass('navbar-col-show');
-  });
+
+    // draw hour
+    ctx.save();
+    var theta = (hour - 3) * 2 * Math.PI / 12;
+    ctx.rotate(theta);
+    ctx.beginPath();
+    ctx.moveTo(-15, -5);
+    ctx.lineTo(-15, 5);
+    ctx.lineTo(clockRadius * 0.5, 1);
+    ctx.lineTo(clockRadius * 0.5, -1);
+    ctx.fill();
+    ctx.restore();
+
+    // draw minute
+    ctx.save();
+    var theta = (minute - 15) * 2 * Math.PI / 60;
+    ctx.rotate(theta);
+    ctx.beginPath();
+    ctx.moveTo(-15, -4);
+    ctx.lineTo(-15, 4);
+    ctx.lineTo(clockRadius * 0.8, 1);
+    ctx.lineTo(clockRadius * 0.8, -1);
+    ctx.fill();
+    ctx.restore();
+
+    // draw second
+    ctx.save();
+    var theta = (seconds - 15) * 2 * Math.PI / 60;
+    ctx.rotate(theta);
+    ctx.beginPath();
+    ctx.moveTo(-15, -3);
+    ctx.lineTo(-15, 3);
+    ctx.lineTo(clockRadius * 0.9, 1);
+    ctx.lineTo(clockRadius * 0.9, -1);
+    ctx.fillStyle = '#0f0';
+    ctx.fill();
+    ctx.restore();
+
+    ctx.restore();
 }
 
-// 头图视差的监听事件
-function parallaxEvent() {
-  var target = $('#background[parallax="true"]');
-  var parallax = function() {
-    var oVal = $(window).scrollTop() / 5;
-    var offset = parseInt($('#board').css('margin-top'), 0);
-    var max = 96 + offset;
-    if (oVal > max) {
-      oVal = max;
-    }
-    target.css({
-      transform          : 'translate3d(0,' + oVal + 'px,0)',
-      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
-      '-ms-transform'    : 'translate3d(0,' + oVal + 'px,0)',
-      '-o-transform'     : 'translate3d(0,' + oVal + 'px,0)'
-    });
+// initialization
+$(function(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
 
-    var toc = $('#toc');
-    if (toc) {
-      $('#toc-ctn').css({
-        'padding-top': oVal + 'px'
-      });
-    }
-  };
-  if (target.length > 0) {
-    listenScroll(parallax);
-  }
-}
+    // var width = canvas.width;
+    // var height = canvas.height;
 
-// 向下滚动箭头的监听事件
-function scrollDownArrowEvent() {
-  $('.scroll-down-bar').on('click', function() {
-    scrollToElement('#board', -$('#navbar').height());
-  });
-}
+clockImage = new Image();
+clockImage.src = 'https://static.runoob.com/images/mix/125855_nnla_89964.png';
 
-// 向顶部滚动箭头的监听事件
-function scrollTopArrowEvent() {
-  var topArrow = $('#scroll-top-button');
-  if (!topArrow) {
-    return;
-  }
-  var posDisplay = false;
-  var scrollDisplay = false;
-  // 位置
-  var setTopArrowPos = function() {
-    var boardRight = document.getElementById('board').getClientRects()[0].right;
-    var bodyWidth = document.body.offsetWidth;
-    var right = bodyWidth - boardRight;
-    posDisplay = right >= 50;
-    topArrow.css({
-      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-      'right' : right - 64 + 'px'
-    });
-  };
-  setTopArrowPos();
-  $(window).resize(setTopArrowPos);
-  // 显示
-  var headerHeight = $('#board').offset().top;
-  listenScroll(function() {
-    var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
-    scrollDisplay = scrollHeight >= headerHeight;
-    topArrow.css({
-      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
-    });
-  });
-  // 点击
-  topArrow.on('click', function() {
-    $('body,html').animate({
-      scrollTop: 0,
-      easing   : 'swing'
-    });
-  });
-}
-
-$(document).ready(function() {
-  navbarScrollEvent();
-  parallaxEvent();
-  scrollDownArrowEvent();
-  scrollTopArrowEvent();
+    setInterval(drawScene, 1000); // loop drawScene
 });
